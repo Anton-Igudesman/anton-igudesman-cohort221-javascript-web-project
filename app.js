17200d7c98ac593c018e4532a66fcbf1bbea51ac
Anton Igudesman <antonguy@gmail.com>
2022-06-26 06:50:42 -0700

    final commit

--- app.js
+++ app.js
@@ -1,206 +1,143 @@
-//arrays used for choosing outfits
-const shoes = ['sneakers', 'sandals', 'winter boots', 'formal shoes', 'galoshes'];
-const pants = ['jeans', 'rain pants', 'casual shorts', 'athletic shorts', 'sweatpants'];
-const shirt = ['you don\'t need a shirt!!', 't-shirt', 'tanktop', 'button-up', 'long-sleeve'];
-const jacket = ['you don\'t need a jacket!!', 'windbreaker', 'blazer', 'rain-coat', 'winter-coat'];
-
-//initializing variable to use for DOM element within functions
-let weather;
-let activity;
-let shoePlan = document.getElementById('shoes');
-let pantsPlan = document.getElementById('pants');
-let shirtPlan = document.getElementById('shirt');
-let jacketPlan = document.getElementById('jacket');
-
-//sunny weather function
-function sun() {
-    let sunClothes = document.getElementById('activity').value;
-    switch(sunClothes) {
-        case 'date':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[2];
-            shirtPlan.innerHTML = shirt[1];
-            jacketPlan.innerHTML = jacket[0]; 
-            break;
-        case 'run':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[3];
-            shirtPlan.innerHTML = shirt[2];
-            jacketPlan.innerHTML = jacket[0];
-        break;
-        case 'garden':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[3];
-            shirtPlan.innerHTML = shirt[0];
-            jacketPlan.innerHTML = jacket[0];
-        break;
-        case 'errands':
-            shoePlan.innerHTML = shoes[1];
-            pantsPlan.innerHTML = pants[2];
-            shirtPlan.innerHTML = shirt[2];
-            jacketPlan.innerHTML = jacket[0];  
-        break;
-        case 'segway':
-            shoePlan.innerHTML = 'Really??!';
-            pantsPlan.innerHTML = 'what is your';
-            shirtPlan.innerHTML = 'obsession with';
-            jacketPlan.innerHTML = 'Segways?!';
-        break;
-        case 'museum':
-            shoePlan.innerHTML = shoes[1];
-            pantsPlan.innerHTML = pants[2];
-            shirtPlan.innerHTML = shirt[1];
-            jacketPlan.innerHTML = jacket[0];
+//clothes constructors
+class Clothes {
+    constructor(bodyPart, type, acceptedActivity, acceptedWeather) {
+        this.bodyPart = bodyPart;
+        this.type = type;
+        this.acceptedActivity = acceptedActivity;
+        this.acceptedWeather = acceptedWeather;
     }
 }
-
-//rainy weather function
-function rain() {
-    let rainGear = document.getElementById('activity').value;
-    switch(rainGear) {
-        case 'date':
-            shoePlan.innerHTML = shoes[4];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[3];
-            jacketPlan.innerHTML = jacket[3]; 
-            break;
-        case 'run':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[1];
-            shirtPlan.innerHTML = shirt[1];
-            jacketPlan.innerHTML = jacket[3];
-        break;
-        case 'garden':
-            shoePlan.innerHTML = shoes[4];
-            pantsPlan.innerHTML = pants[1];
-            shirtPlan.innerHTML = shirt[1];
-            jacketPlan.innerHTML = jacket[3];
-        break;
-        case 'errands':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[4];
-            jacketPlan.innerHTML = jacket[3];  
-        break;
-        case 'segway':
-            shoePlan.innerHTML = 'Really??!';
-            pantsPlan.innerHTML = 'Segways are';
-            shirtPlan.innerHTML = 'not designed';
-            jacketPlan.innerHTML = 'for rain!!';
-        break;
-        case 'museum':
-            shoePlan.innerHTML = shoes[2];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[4];
-            jacketPlan.innerHTML = jacket[4];
-        default:
-            console.log('outfits here');
+class Shoes extends Clothes{
+    constructor(bodyPart, type, acceptedActivity, acceptedWeather, size) {
+        super(bodyPart, type, acceptedActivity, acceptedWeather) 
+        this.size = size;
     }
-}
+};
+class Pants extends Clothes{};
+class Shirt extends Clothes{};
+class Jacket extends Clothes{};
 
-//snow weather function
-function snow() {
-    let snowSuit = document.getElementById('activity').value;
-    switch(snowSuit) {
-        case 'date':
-            shoePlan.innerHTML = shoes[2];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[3];
-            jacketPlan.innerHTML = jacket[4]; 
-            break;
-        case 'run':
-            shoePlan.innerHTML = 'I think';
-            pantsPlan.innerHTML = 'you should relax';
-            shirtPlan.innerHTML = 'and drink';
-            jacketPlan.innerHTML = 'some hot cocoa';
-        break;
-        case 'garden':
-            shoePlan.innerHTML = 'Your garden';
-            pantsPlan.innerHTML = 'is asleep';
-            shirtPlan.innerHTML = 'for the';
-            jacketPlan.innerHTML = 'winter!!';
-        break;
-        case 'errands':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[4];
-            jacketPlan.innerHTML = jacket[4];  
-        break;
-        case 'segway':
-            shoePlan.innerHTML = 'Really??!';
-            pantsPlan.innerHTML = 'Segways are';
-            shirtPlan.innerHTML = 'not designed';
-            jacketPlan.innerHTML = 'for snow!!';
-        break;
-        case 'museum':
-            shoePlan.innerHTML = shoes[2];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[4];
-            jacketPlan.innerHTML = jacket[4];
-        default:
-            console.log('outfits here');
+//shoes
+let sneakers = new Shoes('feet', 'Sneakers', ['running', 'errands', 'segway', 'garden'], 
+['sunny', 'overcast'], 10);
+let sandals = new Shoes('feet', 'Sandals', ['errands', 'segway'], ['sunny'], 11);
+let winterBoots = new Shoes('feet', 'Winter Boots', ['date', 'errands'], ['snowing'], 12);
+let formalShoes = new Shoes('feet', 'Formal Shoes', ['date'], ['sunny', 'rainy', 'overcast'], 10);
+let galoshes = new Shoes('feet', 'Galoshes', ['errands', 'garden'], ['rainy'], 11);
+
+//instantiating pants
+let jeans = new Pants('legs', 'Jeans', ['date', 'errands', 'segway', 'museum'], 
+['sunny', 'rainy', 'snowing', 'overcast']);
+let rainPants = new Pants('legs', 'Gore-Tex Pants', ['errands', 'segway', 'garden'], ['rainy']);
+let casualShorts = new Pants('legs', 'Casual Shorts', ['garden', 'errands', 'segway'], 
+['sunny', 'overcast']);
+let athleticShorts = new Pants('legs', 'Athletic Shorts', ['running', 'garden'], ['sunny', 'overcast']);
+let sweatPants = new Pants('legs', 'Sweat Pants', ['errands', 'garden', 'segway'], ['overcast']);
+
+//instantiating shirts
+let shirtless = new Shirt('torso', 'No Shirt', ['running', 'garden'], ['sunny', 'overcast']);
+let tshirt = new Shirt('torso', 'T-Shirt', ['running', 'garden', 'errands', 'segway'],
+['sunny', 'overcast'])
+let thermal = new Shirt('torso', 'Thermal Shirt', ['errands', 'museum'], ['snowing']);
+let tanktop = new Shirt('torso', 'Tanktop', ['errands', 'garden', 'segway', 'running'], ['sunny']);
+let buttonUp = new Shirt('torso', 'Button Up', ['date', 'museum'], 
+['sunny', 'rainy', 'snowing', 'overcast']);
+let longSleeve = new Shirt('torso', 'Long Sleeve', ['errands', 'segway'], ['overcast']);
+
+//instantiating jackets
+let noJacket = new Jacket('outer-torso', 'No Jacket', 
+['errands', 'running', 'garden', 'museum', 'date', 'segway'], ['sunny']);
+let windBreaker = new Jacket('outer-torso', 'Wind Breaker', 
+['errands', 'running', 'garden', 'segway'], ['overcast']);
+let blazer = new Jacket('outer-torso', 'Blazer', ['date', 'museum'], ['sunny', 'overcast']);
+let rainCoat = new Jacket('outer-torso', 'Rain Coat', ['date', 'errands'], ['rainy']);
+let winterCoat = new Jacket('outer-torso', 'Winter Coat', 
+['date', 'errands', 'museum'], ['snowing']);
+
+//creating empty arrays for different clothes objects to be pushed into for selection
+let shoesArray = [];
+let pantsArray = [];
+let shirtArray = [];
+let jacketArray = [];
+
+//arrays of clothes objects to iterate through
+let shoes = [sneakers, sandals, winterBoots, formalShoes, galoshes];
+let pants = [jeans, rainPants, casualShorts, athleticShorts, sweatPants];
+let shirt = [shirtless, tshirt, thermal, tanktop, buttonUp, longSleeve];
+let jacket = [noJacket, windBreaker, blazer, rainCoat, winterCoat];
+let clothes = [shoes, pants, shirt, jacket];
+
+
+//function to iterate and choose a clothing item of each type
+function clothingPlanner(activity, weather) {
+    activityChoose = document.getElementById('activity').value;
+    weatherChoose = document.getElementById('weather').value;
+    console.log("in function:" + activityChoose);
+    console.log("in function:" + weatherChoose);
+    for (let i = 0; i < clothes.length; i++) {
+        for (let j = 0; j < clothes[i].length; j++) {
+            let clothingKey = clothes[i][j];
+            if (clothingKey.acceptedActivity.indexOf(activity) > -1 
+            && clothingKey.acceptedWeather.indexOf(weather) > -1) {
+                if (clothingKey.bodyPart == 'feet') {
+                    shoesArray.push(clothingKey.type);
+                } else if (clothingKey.bodyPart == 'legs') {
+                    pantsArray.push(clothingKey.type);
+                } else if (clothingKey.bodyPart == 'torso') {
+                    shirtArray.push(clothingKey.type);
+                } else {
+                    jacketArray.push(clothingKey.type);
+                }
+            } 
+        }
     }
 
-    //overcast weather function
-}
-function overcast() {
-    let overcastOutfit = document.getElementById('activity').value;
-    switch(overcastOutfit) {
-        case 'date':
-            shoePlan.innerHTML = shoes[3];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[4];
-            jacketPlan.innerHTML = jacket[2];
-        break;
-        case 'run':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[4];
-            shirtPlan.innerHTML = shirt[1];
-            jacketPlan.innerHTML = jacket[1];
-        break;
-        case 'garden':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[4];
-            shirtPlan.innerHTML = shirt[1];
-            jacketPlan.innerHTML = jacket[0];
-        break;
-        case 'errands':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[4];
-            jacketPlan.innerHTML = jacket[1];  
-        break;
-        case 'segway':
-            shoePlan.innerHTML = 'Really??!';
-            pantsPlan.innerHTML = 'a';
-            shirtPlan.innerHTML = 'SEGWAY';
-            jacketPlan.innerHTML = 'tour???!!';
-        break;
-        case 'museum':
-            shoePlan.innerHTML = shoes[0];
-            pantsPlan.innerHTML = pants[0];
-            shirtPlan.innerHTML = shirt[3];
-            jacketPlan.innerHTML = jacket[0];
-        default:
-            console.log('outfits here');
+    let table = document.getElementById('outfit-table');
+    //if arrays have more than 1 index - returning a random index
+    function clothingPicker(arr1, arr2, arr3, arr4, item1, item2, item3, item4) {
+        if (arr1.length === 0 || arr2.length === 0 || arr3.length === 0 || arr4.length === 0) {
+            table.firstElementChild.innerHTML = 
+            '<td>Your choice of activity is not appropriate for this Weather!</td>';
+        } else {
+            table.innerHTML = '<tr><th>Shoes</th><th>Pants</th><th>Shirt</th><th>Jacket</th></tr><tr><td id="shoes"></td><td id="pants"></td><td id="shirt"></td><td id="jacket"></td></tr>'
+            let shoePlan = document.getElementById(item1);
+            let pantsPlan = document.getElementById(item2);
+            let shirtPlan = document.getElementById(item3);
+            let jacketPlan = document.getElementById(item4);
+            let randomNum1 = Math.floor(Math.random() * arr1.length);
+            let randomNum2 = Math.floor(Math.random() * arr2.length);
+            let randomNum3 = Math.floor(Math.random() * arr3.length);
+            let randomNum4 = Math.floor(Math.random() * arr4.length);
+            shoePlan.innerHTML = arr1[randomNum1];
+            pantsPlan.innerHTML = arr2[randomNum2];
+            shirtPlan.innerHTML = arr3[randomNum3];
+            jacketPlan.innerHTML = arr4[randomNum4];
+        }
     }
+    clothingPicker(shoesArray, pantsArray, shirtArray, jacketArray, 'shoes', 'pants', 'shirt', 'jacket');
+    shoesArray = [];
+    pantsArray = [];
+    shirtArray = [];
+    jacketArray = [];
 }
 
-//click event function for Plan My Outfit button
-function outfits() {
-    weather = document.getElementById('weather').value;
-    if(weather == 'overcast') {
-        overcast();
-    } else if(weather == 'snowing') {
-        snow();
-    } else if(weather == 'rainy') {
-        rain();
-    } else if(weather == 'sunny') {
-        sun();
-    } else {
-        console.log('pick something ya bastard!!')
-    }
-}
+
+//event listener for Plan My Outfit 'click'let activityChoose;
+let weatherChoose;
+let activityChoose;
+
+document.getElementById('outfit').addEventListener('click', function () {
+    activityChoose = document.getElementById('activity').value;
+    weatherChoose = document.getElementById('weather').value;
+    console.log(activityChoose);
+    console.log(weatherChoose);
+    clothingPlanner(activityChoose, weatherChoose)
+});
+
+
+
+  
+       
     
-//event listener for Plan My Outfit 'click'
-document.getElementById('outfit').addEventListener('click', outfits);
+
+
